150a33d46e86cdc85b1dca0c7b9ef5c143a43179
Thierry Halot <HalotThierry34@gmail.com>
2021-11-25 17:51:52 +0100

    finalisation of the exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { SolutionComponent } from './solution/solution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    InstructionsComponent,
+    SolutionComponent
   ],
   imports: [
     BrowserModule
